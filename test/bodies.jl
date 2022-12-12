de4963a8efb742cc381166f6dbbe9573482f2a58
Jeff Eldredge <jdeldre@g.ucla.edu>
2022-12-12 10:52:18 -0800

    Loosened some tests

--- test/bodies.jl
+++ test/bodies.jl
@@ -22,7 +22,7 @@ const MYEPS = 20*eps()
 
   s = arccoord(c)
   @test s[end] ≈ 12.0
-  @test s[end] == arclength(c)
+  @test s[end] ≈ arclength(c)
   smid = arccoordmid(c)
   @test smid[1] > 0.0 && smid[end] < s[end]
 
