dfb47d2854b7c6865404a7cbf14e7d04e7f63b46
Jeff Eldredge <jdeldre@g.ucla.edu>
2022-12-13 12:07:36 -0800

    Extended surface_velocity for lists

--- src/lists.jl
+++ src/lists.jl
@@ -240,30 +240,36 @@ end
 
 """
     surface_velocity!(u::AbstractVector{Float64},v::AbstractVector{Float64},
-                     bl::BodyList,ml::MotionList,t::Real)
+                     bl::BodyList,ml::MotionList,t::Real[;inertial=true])
 
 Assign the components of velocity `u` and `v` (in inertial coordinate system)
 at surface positions described by coordinates inertial coordinates in each body in `bl` at time `t`,
-based on supplied motions in the MotionList `ml` for each body.
+based on supplied motions in the MotionList `ml` for each body.  If, instead,
+`inertial=false`, then it is assumed that `u`, `v`, and body coordinates are all
+expressed in comoving coordinates for each body (but velocities are relative to inertial
+  frame).
 """
 function surface_velocity!(u::AbstractVector{Float64},v::AbstractVector{Float64},
-                 bl::BodyList,ml::MotionList,t::Real)
+                 bl::BodyList,ml::MotionList,t::Real;inertial=true)
 
    for i in 1:length(bl)
-      surface_velocity!(view(u,bl,i),view(v,bl,i),bl[i],ml[i],t)
+      surface_velocity!(view(u,bl,i),view(v,bl,i),bl[i],ml[i],t;inertial=inertial)
    end
    return u, v
 end
 
 """
-    surface_velocity(bl::BodyList,ml::MotionList,t::Real)
+    surface_velocity(bl::BodyList,ml::MotionList,t::Real[;inertial=true])
 
 Return the components of rigid body velocity (in inertial coordinate system)
 at surface positions described by coordinates inertial coordinates in each body in `bl` at time `t`,
-based on supplied motions in the MotionList `ml` for each body.
+based on supplied motions in the MotionList `ml` for each body.   If, instead,
+`inertial=false`, then it is assumed that velocities and body coordinates are all
+expressed in comoving coordinates for each body (but velocities are relative to inertial
+  frame).
 """
-surface_velocity(bl::BodyList,ml::MotionList,t::Real) =
-    surface_velocity!(zeros(Float64,numpts(bl)),zeros(Float64,numpts(bl)),bl,ml,t)
+surface_velocity(bl::BodyList,ml::MotionList,t::Real;inertial=true) =
+    surface_velocity!(zeros(Float64,numpts(bl)),zeros(Float64,numpts(bl)),bl,ml,t;inertial=inertial)
 
 
 """
