ac39754761a1f403e6abd6af69c6262fd34e42b0
Jeff Eldredge <jdeldre@g.ucla.edu>
2023-06-27 20:34:06 -0700

    Added a few simplified constructors for transforms

--- src/rigidtransform.jl
+++ src/rigidtransform.jl
@@ -8,6 +8,7 @@
 const O3VECTOR = SVector{3}(zeros(Float64,3))
 const I3 = SMatrix{3,3}(I)
 const O3 = SMatrix{3,3}(zeros(Float64,9))
+const I6 = SMatrix{6,6}(I)
 
 const PLUCKER2DANG = 1:1
 const PLUCKER2DLIN = 2:3
@@ -378,6 +379,32 @@ function _force_transform_matrix(xA_to_B::SVector{3},RA_to_B::SMatrix{3,3})
     return Mrot*Mtrans
 end
 
+function _motion_transform_matrix(xA_to_B::SVector{3})
+    xM = cross_matrix(xA_to_B)
+    Mtrans = SMatrix{6,6}([I3 O3;
+                          -xM I3])
+    return Mtrans
+end
+
+function _force_transform_matrix(xA_to_B::SVector{3})
+    xM = cross_matrix(xA_to_B)
+    Mtrans = SMatrix{6,6}([I3 -xM;
+                           O3 I3])
+    return Mtrans
+end
+
+function _motion_transform_matrix(RA_to_B::SMatrix{3,3})
+    Mrot = SMatrix{6,6}([RA_to_B O3;
+                         O3 RA_to_B])
+    return Mrot
+end
+
+function _force_transform_matrix(RA_to_B::SMatrix{3,3})
+    Mrot = SMatrix{6,6}([RA_to_B O3;
+                          O3   RA_to_B])
+    return Mrot
+end
+
 for f in [:motion, :force]
 
     fname_underscore = Symbol("_",f,"_transform_matrix")
@@ -401,6 +428,12 @@ for f in [:motion, :force]
         x_3d = SVector{3}([x_2d... 0.0])
         M = $fname2d_underscore(x_3d,R_3d)
         return $typename{2}(x_3d,R_3d,M)
+    end
+
+    @eval function $typename(x_2d::SVector{2})
+        x_3d = SVector{3}([x_2d... 0.0])
+        M = $fname2d_underscore(x_3d)
+        return $typename{2}(x_3d,I3,M)
     end
 
     @eval function $typename(x_2d::SVector{2},R_2d::SMatrix{2,2})
@@ -433,6 +466,16 @@ for f in [:motion, :force]
 
     @eval function $fname2d_underscore(x_3d::SVector{3},R_3d::SMatrix{3,3})
         M_3d = $fname_underscore(x_3d,R_3d)
+        return SMatrix{3,3}(M_3d[3:5,3:5])
+    end
+
+    @eval function $fname2d_underscore(x_3d::SVector{3})
+        M_3d = $fname_underscore(x_3d)
+        return SMatrix{3,3}(M_3d[3:5,3:5])
+    end
+
+    @eval function $fname2d_underscore(R_3d::SMatrix{3,3})
+        M_3d = $fname_underscore(R_3d)
         return SMatrix{3,3}(M_3d[3:5,3:5])
     end
 
